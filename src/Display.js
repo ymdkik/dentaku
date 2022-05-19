1165bd259b130eb9e63f0c78ebbddbb2ec028207
山田 海輝 <kaiki.yamada@thinkings.co.jp>
2022-05-19 18:07:39 +0900

    display migiyose

--- src/Display.js
+++ src/Display.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { textC, setTextC } from './Calculation'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import './Display.css';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -9,6 +10,9 @@ const useStyles = makeStyles((theme) => ({
             margin: "10px 100px",
             width: 300,
         },
+    },
+    text: {
+        textAlign: "right",
     }
 }));
 
@@ -19,7 +23,7 @@ function Display() {
 
     return (
         <div className={classes.root} noValidate autoComplete="off">
-            <TextField label="電卓" id="outlined-size-normal" value={text[text.length-1]} variant="outlined"/>
+            <TextField className={classes.text} label="電卓" id="outlined-size-normal" value={text[text.length-1]} variant="outlined"/>
         </div>
     )
 }
